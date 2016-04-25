63e1907617d4a2cefddf459c33463d4a5cc1f123
ZitongZhang <henry921224@gmail.com>
2016-04-25 15:42:17 -0400

    Using worker pool

--- worker/worker.py
+++ worker/worker.py
@@ -1,9 +1,8 @@
 import json
-import time
-from codecs import open
 import boto3
 from alchemyapi import AlchemyAPI
 from create import create_topic
+from multiprocessing import Pool
 
 alchemyapi = AlchemyAPI()
 
@@ -12,28 +11,21 @@ queue = sqs.get_queue_by_name(QueueName='TwittMap')
 topic = create_topic()
 
 
-def appendlog(f, s):
-    f.write(u'[{0}] {1}\n'.format(time.strftime('%Y-%m-%dT%H:%M:%SZ'), s))
-    f.flush()
+def worker(_):
+    while True:
+        for message in queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=20):
+            try:
+                tweet = json.loads(message.body)
+                response = alchemyapi.sentiment('text', tweet['text'])
+                if response['status'] == 'OK':
+                    tweet['sentiment'] = response['docSentiment']['type']
+                    encoded = json.dumps(tweet, ensure_ascii=False)
+                    # Push to Amazon SNS
+                    topic.publish(Message=encoded)
+            finally:
+                message.delete()
 
 
 if __name__ == '__main__':
-    with open('worker.log', 'a', encoding='utf8') as f:
-        appendlog(f, 'Program starts')
-        while True:
-            for message in queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=20):
-                try:
-                    tweet = json.loads(message.body)
-                    response = alchemyapi.sentiment('text', tweet['text'])
-                    if response['status'] == 'OK':
-                        tweet['sentiment'] = response['docSentiment']['type']
-                        encoded = json.dumps(tweet, ensure_ascii=False)
-                        # Push to Amazon SNS
-                        topic.publish(Message=encoded)
-                        appendlog(f, '{0}: {1}'.format(tweet['sentiment'], tweet['text']))
-                    else:
-                        appendlog(f, 'Analysis failed: {0}'.format(tweet['text']))
-                except Exception as e:
-                    appendlog(f, '{0}: {1}'.format(type(e), str(e)))
-                finally:
-                    message.delete()
+    pool = Pool(3)
+    pool.map(worker, range(3))
